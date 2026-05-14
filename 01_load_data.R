26b91817f7af9ef652ea05a2590926549aaf7c65
Genevieve Perkins <genevieve.c.perkins@gmail.com>
2026-05-13 20:38:00 -0700

    update loading data

--- 01_load_data.R
+++ 01_load_data.R
@@ -40,135 +40,79 @@ write.csv(ref, fs::path("01_clean_data", "reference.csv") )
 
 
 
-# read in the dead tags #####################################
-# check if this matches the counts for all transmission download 
 
-tag_dir <-  fs::path(data_dir, "Dead Sheep Data")          
-dtags <- list.files(tag_dir, pattern = "txt", full.names = TRUE)
+############################################
+## Summary of tags 
 
-# read in each file and join togehter 
-dead_tags <- purrr::map_dfr(dtags, function(x){
-  #x <- dtags[1]
-  tfile <- read.table(fs::path(x), header = T, sep = ",") |> 
-    mutate(tag_id = as.character(CollarSerialNumber )) |>
-    mutate(Date = ymd(Date)) 
+## manually edited this file
 
-}) |> bind_rows()
+ref <- read.csv(fs::path("01_clean_data", "reference_edit.csv"))
+ref <- ref |> 
+  mutate(tag_idn = gsub("-", "", tag_id))
   
-# summary 
-dtag_sum <- dead_tags |> 
-  group_by(tag_id) |> 
-  count()
-
-# confirmed that this matches the tags in the total transmission data so will use this source 
-
-# get last date for each of these tags to update ref 
-last_date <- dead_tags |> 
-  select(Date, tag_id) |> 
-  group_by(tag_id) |> 
-  summarise(max(Date))
-
-
-# plot the frequency to check for breaks 
-ggplot(dead_tags, aes(x = Date, y = Temperature)) +
-  geom_line() +
-  facet_wrap(~tag_id)+
-  #scale_x_date(date_labels = "%b %Y")
-  scale_x_date(date_breaks = "3 month", date_labels = "%b %d %Y")
-
-
-
-
-#### 55680 #######
-s1 <- dead_tags |> 
-  dplyr::filter(tag_id == 55680)
-
-# plot the frequency to check for breaks 
-ggplot(s1, aes(x = Date, y = Temperature)) +
-  geom_line() +
-  #scale_x_date(date_labels = "%b %Y")
-  scale_x_date(date_breaks = "3 month", date_labels = "%b %d %Y")
-
-
-## checked this record to find the break
-s1_dates <- s1 |> 
-  select(Date, tag_id) |> 
-  group_by(Date,tag_id) |> 
-  count()
-
-# 
-ggplot(s1_dates, aes(x = Date, y = n)) +
-  geom_line() +
-  #scale_x_date(date_labels = "%b %Y")
-  scale_x_date(date_breaks = "3 month", date_labels = "%b %d %Y") 
-
-## questions 
-# for 55680-1 and 55680-2 both tagged within short time frame, 
-# tag freq slow down around March 27 and March 28 (potential there is UTC time?)
-# then tag swapped? and then redeployed? 
-# Q: was 55680-1 a dud? 
-
-
-#### 55686 #######
-s2 <- dead_tags |> 
-  dplyr::filter(tag_id == 55686)
-
-# plot the frequency to check for breaks 
-ggplot(s2, aes(x = Date, y = Temperature)) +
-  geom_line() +
-  #scale_x_date(date_labels = "%b %Y")
-  scale_x_date(date_breaks = "3 month", date_labels = "%b %d %Y")
-
-
-## checked this record to find the break
-s2_dates <- s2 |> 
-  select(Date, tag_id) |> 
-  group_by(Date,tag_id) |> 
-  count()
-
-# 
-ggplot(s2_dates, aes(x = Date, y = n)) +
-  geom_line() +
-  #scale_x_date(date_labels = "%b %Y")
-  scale_x_date(date_breaks = "3 month", date_labels = "%b %d %Y") 
-
-
-## seems something funcky with this tag as the frequency of 
-# counts seemed to drop at the end before tag died. 
-
-
-
-
-
-
-
-# read in the location dates 
-# update this to latest folder 
-latest_dir <-  fs::path(data_dir, "20260325")          
-
-
-list.files(latest_dir)       
-
-loc <- read.table(fs::path(latest_dir, "Cumulative_D_13197_202623181141.txt"), header = T, sep = ",")
-
-
-
-# summary 
-tag_sum <- loc |> 
-  group_by(CollarSerialNumber) |> 
-  count() |> 
-  mutate(tag_id = as.character(CollarSerialNumber)) 
-
-tag_date <- loc |> 
-  group_by(CollarSerialNumber) |> 
-  slice_max(Date) |> 
-  select(CollarSerialNumber, Date ) |> 
-  unique()
-
-
-
-check <- left_join(tag_sum, dtag_sum)
-
-
-
   
+## summary of tags 
+
+# by sex 
+sex_sum <- ref |> 
+  group_by(sex) |> 
+  count()
+
+sex_sum
+
+
+# by age class and sex
+sex_age_sum <- ref |> 
+  group_by(sex, sheep_class) |> 
+  count()
+
+sex_age_sum
+
+
+# by age annualli and sex
+sex_aage_sum <- ref |> 
+  group_by(sex, Age_annuli) |> 
+  count()
+
+sex_aage_sum
+
+
+
+# duration of the tags 
+dur <- ref |> 
+  mutate(end_date_estimate = case_when(
+    is.na(End_Date) ~ "2026-04-07",
+    .default = End_Date
+  )) |> 
+  mutate(end_date_estimate = ymd(end_date_estimate)) |> 
+  mutate(capture_date = ymd(capture_date)) |> 
+  mutate(duration = end_date_estimate - capture_date)
+
+
+
+dur_plot <- ggplot(dur, aes(y=factor(tag_idn), color = sex)) +  
+  geom_segment(aes(x=capture_date, xend=end_date_estimate, y=factor(tag_idn), yend=factor(tag_idn)), size=1)+  
+  xlab("Date") + ylab("Tag") +
+  scale_color_viridis_d(begin = 0.2, end = 0.8)+
+  facet_wrap(~ sheep_class) #+
+# xlim("2022-01-01", "2026-07-04")
+
+
+dur_plot  
+
+dur_hist <- ggplot(dur, aes(x= duration))+  
+  geom_histogram() + #fill="white", position="dodge") +  
+  scale_color_viridis_d()+
+  #facet_wrap(~year)+  
+  xlab("duration (days)")   
+
+dur_hist  
+
+
+
+## Lambing dates 3rd May - June 14 (enns et al. 2024)
+# 
+# out <- out %>%
+#   group_by(tag.id) |> 
+#   mutate(diff = difftime(date_time, lag(date_time),  units = c("hours")), 
+#          diff = as.numeric(diff)) 
